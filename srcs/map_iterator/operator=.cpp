2d898fbb00929ca9c0f47f8f9bee79351098adb4
Remy GENY <rgeny@e1r10p7.clusters.42paris.fr>
2022-10-05 17:58:57 +0200

    map_iterator: add test for operator=

---
+++ srcs/map_iterator/operator=.cpp
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operator=.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/05 17:46:57 by rgeny             #+#    #+#             */
+/*   Updated: 2022/10/05 17:56:40 by rgeny            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_map.hpp"
+
+typedef NAMESPACE::map<int, int>	MAP;
+typedef MAP::iterator				ITERATOR;
+typedef MAP::const_iterator			CONST_ITERATOR;
+
+int	main	(void)
+{
+	MAP				m;
+
+	m[31] = 7;
+
+	ITERATOR		it_empty,
+					it = m.begin();
+	CONST_ITERATOR	cit_empty,
+					cit = m.begin();
+	
+	PRINT_EXECUTE(it_empty == it_empty);
+	PRINT_EXECUTE(it_empty == it);
+	PRINT_EXECUTE(it_empty == cit_empty);
+	PRINT_EXECUTE(it_empty == cit);
+	_print_nl();
+
+	PRINT_EXECUTE(it == it_empty);
+	PRINT_EXECUTE(it == it);
+	PRINT_EXECUTE(it == cit_empty);
+	PRINT_EXECUTE(it == cit);
+	_print_nl();
+
+	PRINT_EXECUTE(cit_empty == it_empty);
+	PRINT_EXECUTE(cit_empty == it);
+	PRINT_EXECUTE(cit_empty == cit_empty);
+	PRINT_EXECUTE(cit_empty == cit);
+	_print_nl();
+
+	PRINT_EXECUTE(cit == it_empty);
+	PRINT_EXECUTE(cit == it);
+	PRINT_EXECUTE(cit == cit_empty);
+	PRINT_EXECUTE(cit == cit);
+	_print_nl();
+
+	ITERATOR		it_cpy_it_empty,
+					it_cpy_it;
+	CONST_ITERATOR	cit_cpy_it_empty,
+					cit_cpy_it,
+					cit_cpy_cit_empty,
+					cit_cpy_cit;
+	
+	it_cpy_it_empty = it_empty;
+	it_cpy_it = it;
+//	it_cpy_cit_empty = cit_empty;
+//	it_cpy_cit = cit;				//crash_test
+	cit_cpy_it_empty = it_empty;
+	cit_cpy_it = it;
+	cit_cpy_cit_empty = cit_empty;
+	cit_cpy_cit = cit;
+
+
+	PRINT_EXECUTE(it_cpy_it_empty == it_empty);
+	PRINT_EXECUTE(it_cpy_it_empty == it);
+	PRINT_EXECUTE(it_cpy_it_empty == cit_empty);
+	PRINT_EXECUTE(it_cpy_it_empty == cit);
+	PRINT_EXECUTE(it_cpy_it_empty == it_cpy_it_empty);
+	PRINT_EXECUTE(it_cpy_it_empty == it_cpy_it);
+	PRINT_EXECUTE(it_cpy_it_empty == cit_cpy_it_empty);
+	PRINT_EXECUTE(it_cpy_it_empty == cit_cpy_it);
+	PRINT_EXECUTE(it_cpy_it_empty == cit_cpy_cit_empty);
+	PRINT_EXECUTE(it_cpy_it_empty == cit_cpy_cit);
+	_print_nl();
+
+	PRINT_EXECUTE(it_cpy_it == it_empty);
+	PRINT_EXECUTE(it_cpy_it == it);
+	PRINT_EXECUTE(it_cpy_it == cit_empty);
+	PRINT_EXECUTE(it_cpy_it == cit);
+	PRINT_EXECUTE(it_cpy_it == it_cpy_it_empty);
+	PRINT_EXECUTE(it_cpy_it == it_cpy_it);
+	PRINT_EXECUTE(it_cpy_it == cit_cpy_it_empty);
+	PRINT_EXECUTE(it_cpy_it == cit_cpy_it);
+	PRINT_EXECUTE(it_cpy_it == cit_cpy_cit_empty);
+	PRINT_EXECUTE(it_cpy_it == cit_cpy_cit);
+	_print_nl();
+
+	PRINT_EXECUTE(cit_cpy_it_empty == it_empty);
+	PRINT_EXECUTE(cit_cpy_it_empty == it);
+	PRINT_EXECUTE(cit_cpy_it_empty == cit_empty);
+	PRINT_EXECUTE(cit_cpy_it_empty == cit);
+	PRINT_EXECUTE(cit_cpy_it_empty == it_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_it_empty == it_cpy_it);
+	PRINT_EXECUTE(cit_cpy_it_empty == cit_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_it_empty == cit_cpy_it);
+	PRINT_EXECUTE(cit_cpy_it_empty == cit_cpy_cit_empty);
+	PRINT_EXECUTE(cit_cpy_it_empty == cit_cpy_cit);
+	_print_nl();
+
+	PRINT_EXECUTE(cit_cpy_it == it_empty);
+	PRINT_EXECUTE(cit_cpy_it == it);
+	PRINT_EXECUTE(cit_cpy_it == cit_empty);
+	PRINT_EXECUTE(cit_cpy_it == cit);
+	PRINT_EXECUTE(cit_cpy_it == it_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_it == it_cpy_it);
+	PRINT_EXECUTE(cit_cpy_it == cit_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_it == cit_cpy_it);
+	PRINT_EXECUTE(cit_cpy_it == cit_cpy_cit_empty);
+	PRINT_EXECUTE(cit_cpy_it == cit_cpy_cit);
+	_print_nl();
+
+	PRINT_EXECUTE(cit_cpy_cit_empty == it_empty);
+	PRINT_EXECUTE(cit_cpy_cit_empty == it);
+	PRINT_EXECUTE(cit_cpy_cit_empty == cit_empty);
+	PRINT_EXECUTE(cit_cpy_cit_empty == cit);
+	PRINT_EXECUTE(cit_cpy_cit_empty == it_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_cit_empty == it_cpy_it);
+	PRINT_EXECUTE(cit_cpy_cit_empty == cit_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_cit_empty == cit_cpy_it);
+	PRINT_EXECUTE(cit_cpy_cit_empty == cit_cpy_cit_empty);
+	PRINT_EXECUTE(cit_cpy_cit_empty == cit_cpy_cit);
+	_print_nl();
+
+	PRINT_EXECUTE(cit_cpy_cit == it_empty);
+	PRINT_EXECUTE(cit_cpy_cit == it);
+	PRINT_EXECUTE(cit_cpy_cit == cit_empty);
+	PRINT_EXECUTE(cit_cpy_cit == cit);
+	PRINT_EXECUTE(cit_cpy_cit == it_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_cit == it_cpy_it);
+	PRINT_EXECUTE(cit_cpy_cit == cit_cpy_it_empty);
+	PRINT_EXECUTE(cit_cpy_cit == cit_cpy_it);
+	PRINT_EXECUTE(cit_cpy_cit == cit_cpy_cit_empty);
+	PRINT_EXECUTE(cit_cpy_cit == cit_cpy_cit);
+	_print_nl();
+}

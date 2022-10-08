0aa505c4afb4a16a5d8eda5c7ae2ebc00870d74f
Remy GENY <rgeny@e1r2p1.clusters.42paris.fr>
2022-10-08 15:03:48 +0200

    reverse_iterator: add operator<< for pair in header

--- includes/test_reverse_iterator.hpp
+++ includes/test_reverse_iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 15:21:52 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/04 15:28:12 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/08 15:01:43 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,43 @@
 
 # ifdef FT
 #  include "vector.hpp"
+#  include "map.hpp"
+template
+<
+	typename T1,
+	typename T2
+>
+std::ostream &	operator<<	(std::ostream & os,
+							 ft::pair<T1, T2> const & p)
+{
+	os	<< "("
+		<< p.first
+		<< ":"
+		<< p.second
+		<< ")";
+	return (os);
+}
 # else
 #  include <vector>
+#  include <map>
 # endif
+
+template
+<
+	typename T1,
+	typename T2
+>
+std::ostream &	operator<<	(std::ostream & os,
+							 std::pair<T1, T2> const & p)
+{
+	os	<< "("
+		<< p.first
+		<< ":"
+		<< p.second
+		<< ")";
+	return (os);
+}
+
 
 #endif
 

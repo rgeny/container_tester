aa50f23cbdf29a32308d10dc4a798688646e47cb
Remy GENY <rgeny@e1r6p3.clusters.42paris.fr>
2022-10-10 16:25:02 +0200

    map: add test in insert.cpp

--- srcs/map/modifiers/insert.cpp
+++ srcs/map/modifiers/insert.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 16:24:33 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/09 15:07:41 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/10 16:24:03 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,13 +46,32 @@ int	main	(void)
 	_print_nl();
 	_print_nl("iterator insert(iterator hint, const value_type & value");
 	it = m.begin();
-	for (int i = 0; i < SIZE_POS; ++i)
+	for (int i = SIZE_POS; i > SIZE_POS / 2; --i)
 	{
-		MAP::value_type		map_pair(i, -i);
+		MAP::value_type		map_pair(i, -i),
+							map_pair2(i, i + 1);
 		it = m.insert(it, map_pair);
 		PRINT_EXECUTE(it->first);
 		PRINT_EXECUTE(it->second);
+		it = m.insert(it, map_pair2);
+		PRINT_EXECUTE(it->first);
+		PRINT_EXECUTE(it->second);
 	}
+
+	for (int i = SIZE_POS / 2; i > 0; --i)
+	{
+		MAP::value_type		map_pair(i, -i),
+							map_pair2(i, i + 1);
+		it = m.end();
+		it = m.insert(it, map_pair);
+		PRINT_EXECUTE(it->first);
+		PRINT_EXECUTE(it->second);
+		it = m.end();
+		it = m.insert(it, map_pair2);
+		PRINT_EXECUTE(it->first);
+		PRINT_EXECUTE(it->second);
+	}
+	print_map(m);
 
 	_print_nl();
 	_print_nl();

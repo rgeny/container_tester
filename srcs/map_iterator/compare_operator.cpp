5bf9a14ce578fb29eb410c340f3c6dcb14ff46cb
Remy GENY <rgeny@e1r6p1.clusters.42paris.fr>
2022-10-18 18:16:24 +0200

    map_iterator: compare_operator is now test in const function

--- srcs/map_iterator/compare_operator.cpp
+++ srcs/map_iterator/compare_operator.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 13:26:29 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/08 13:35:54 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 18:15:50 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,11 @@
 
 typedef NAMESPACE::map<int, int>	MAP;
 
-int	main	(void)
+template < typename Container >
+void	Test<Container>::test	(void)
 {
-	MAP				m;
-	for (int i = 0; i < SIZE; ++i)
-		m[i] = i;
-
-	MAP::iterator	it = m.begin(),
-					ite= m.end();
+	MAP::iterator	it = this->begin(),
+					ite= this->end();
 
 	for (int i = 0; i < SIZE * 2; ++i, ++it)
 	{
@@ -32,4 +29,29 @@ int	main	(void)
 		PRINT_EXECUTE(it != ite);
 		_print_nl();
 	}
+}
+template < typename Container >
+void	Test<Container>::const_test	(void) const
+{
+	MAP::const_iterator	it	= this->begin(),
+						ite	= this->end();
+
+	for (int i = 0; i < SIZE * 2; ++i, ++it)
+	{
+		PRINT_EXECUTE(i);
+		PRINT_EXECUTE(it == ite);
+		PRINT_EXECUTE(it != ite);
+		_print_nl();
+	}
+}
+
+int	main	(void)
+{
+	Test<MAP>	m;
+
+	for (int i = 0; i < SIZE; ++i)
+		m[i] = i;
+	
+	m.test();
+	m.const_test();
 }

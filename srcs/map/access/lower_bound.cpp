c41f98d55c6ec9b3899eb5dc96e2aa928a5f0882
Remy GENY <rgeny@e1r6p1.clusters.42paris.fr>
2022-10-18 17:00:54 +0200

    map: lower bound is now test in const function

--- srcs/map/access/lower_bound.cpp
+++ srcs/map/access/lower_bound.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:15:15 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/05 14:17:44 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 17:00:32 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,46 @@
 
 typedef NAMESPACE::map<int, int>	MAP;
 
-int	main	(void)
+template < typename Container >
+void	Test<Container>::test	(void)
 {
-	MAP						m;
 	MAP::iterator			it;
-	MAP::const_iterator		cit;
 
-	for (int i = 0; i < SIZE; ++i)
-		m[i] = i;
-	
 	for (int i = -2; i < SIZE + 2; ++i)
 	{
 		int const	val = i;
 
-		it = m.lower_bound(val);
-		cit= m.lower_bound(val);
+		it = this->lower_bound(val);
 
 		PRINT_EXECUTE(it->first);
 		PRINT_EXECUTE(it->second);
-		PRINT_EXECUTE(cit->first);
-		PRINT_EXECUTE(cit->second);
 		_print_nl();
 	}
+}
+template < typename Container >
+void	Test<Container>::const_test	(void) const
+{
+	MAP::const_iterator		it;
+
+	for (int i = -2; i < SIZE + 2; ++i)
+	{
+		int const	val = i;
+
+		it = this->lower_bound(val);
+
+		PRINT_EXECUTE(it->first);
+		PRINT_EXECUTE(it->second);
+		_print_nl();
+	}
+}
+
+int	main	(void)
+{
+	Test<MAP>	m;
+
+	for (int i = 0; i < SIZE; ++i)
+		m[i] = i;
+
+	m.test();
+	m.const_test();
 }

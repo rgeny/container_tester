8c97cc4fc4e0049f8eb85f76facc44c824ff4587
Remy GENY <rgeny@e1r6p1.clusters.42paris.fr>
2022-10-18 16:15:16 +0200

    map: get_allocator is now test in const function

--- srcs/map/access/get_allocator.cpp
+++ srcs/map/access/get_allocator.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 12:51:00 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/03 12:55:51 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 16:14:14 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,21 @@
 
 typedef NAMESPACE::map<int, int, std::less<int>, std::allocator<long> >	MAP;
 
+template < typename Container >
+void	Test<Container>::const_test	(void) const
+{
+	typename Container::allocator_type	alloc = this->get_allocator();
+
+	PRINT_EXECUTE(this->max_size());
+	PRINT_EXECUTE(alloc.max_size());
+}
+
 int	main	(void)
 {
-	MAP						m;
+	Test<MAP>	m;
 
 	for (int i = 0; i < SIZE; ++i)
 		m[i * SIZE] = -i * i;
-	
-	MAP::allocator_type	alloc = m.get_allocator();
 
-	PRINT_EXECUTE(m.max_size());
-	PRINT_EXECUTE(alloc.max_size());
+	m.const_test();
 }

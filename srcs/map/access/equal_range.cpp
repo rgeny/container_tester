2c4f325ce842092c80c1dcd559a8353f231a62d6
Remy GENY <rgeny@e1r6p1.clusters.42paris.fr>
2022-10-18 16:58:16 +0200

    map: equal range is now test in const function

--- srcs/map/access/equal_range.cpp
+++ srcs/map/access/equal_range.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:06:15 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/05 14:13:51 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 16:57:48 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,28 @@ typedef NAMESPACE::map<int, int>									MAP;
 typedef NAMESPACE::pair<MAP::iterator, MAP::iterator>				IT_PAIR;
 typedef NAMESPACE::pair<MAP::const_iterator, MAP::const_iterator>	CIT_PAIR;
 
-int	main	(void)
+template < typename Container >
+void	Test<Container>::test	(void)
 {
-	MAP			m;
-	
-	for (int i = 0; i < SIZE; ++i)
-	{
-		m[i] = i;
-		m.insert(MAP::value_type(i, -i - 4));
-	}
-	
 	for (int i = -2; i < SIZE + 2; ++i)
 	{
 		int const	val = i;
-		IT_PAIR		it = m.equal_range(val);
-		CIT_PAIR	cit= m.equal_range(val);
+		IT_PAIR		it = this->equal_range(val);
 
 		PRINT_EXECUTE(it.first->first);
 		PRINT_EXECUTE(it.first->second);
 		PRINT_EXECUTE(it.second->first);
 		PRINT_EXECUTE(it.second->second);
+		_print_nl();
+	}
+}
+template < typename Container >
+void	Test<Container>::const_test	(void) const
+{
+	for (int i = -2; i < SIZE + 2; ++i)
+	{
+		int const	val = i;
+		CIT_PAIR	cit= this->equal_range(val);
 
 		PRINT_EXECUTE(cit.first->first);
 		PRINT_EXECUTE(cit.first->second);
@@ -45,4 +47,18 @@ int	main	(void)
 		PRINT_EXECUTE(cit.second->second);
 		_print_nl();
 	}
+}
+
+int	main	(void)
+{
+	Test<MAP>			m;
+	
+	for (int i = 0; i < SIZE; ++i)
+	{
+		m[i] = i;
+		m.insert(MAP::value_type(i, -i - 4));
+	}
+
+	m.test();
+	m.const_test();
 }

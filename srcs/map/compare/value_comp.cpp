c362d951d455240081faeb0b6c3a2c52bf4fb259
Remy GENY <rgeny@e1r6p1.clusters.42paris.fr>
2022-10-18 17:07:21 +0200

    map: value_comp is now test in const function

--- srcs/map/compare/value_comp.cpp
+++ srcs/map/compare/value_comp.cpp
@@ -6,7 +6,7 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:22:18 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/05 16:23:47 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/18 17:06:58 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,30 @@
 
 typedef NAMESPACE::map<int, std::string>	MAP;
 
-int	main	(void)
+template < typename Container >
+void	Test<Container>::const_test	(void) const
 {
-	MAP		m;
-	MAP::value_compare	value_comp = m.value_comp();
-	for (int i = 0; i < SIZE; ++i)
-		m[i] = 'a' + i;
-	
-	for (MAP::iterator it = m.begin(), ite = m.end();
+	MAP::value_compare	value_comp = this->value_comp();
+
+	for (MAP::const_iterator it = this->begin(), ite = this->end();
 		 it != ite;
 		 ++it)
 	{
-		for (MAP::iterator it2 = m.begin(), ite2 = m.end();
+		for (MAP::const_iterator it2 = this->begin(), ite2 = this->end();
 			 it2 != ite2;
 			 ++it2)
 		{
 			PRINT_EXECUTE(value_comp(*it, *it2));
 		}
 	}
+}
+
+int	main	(void)
+{
+	Test<MAP>		m;
+
+	for (int i = 0; i < SIZE; ++i)
+		m[i] = 'a' + i;
+
+	m.const_test();
 }

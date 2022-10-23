3fc2f6a41181ad01c50ca363c33bccac20d088c9
Remy GENY <rgeny@e1r10p5.clusters.42paris.fr>
2022-10-23 17:53:41 +0200

    set: add test for swap

--- srcs/set/modifiers/swap.cpp
+++ srcs/set/modifiers/swap.cpp
@@ -6,59 +6,59 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 13:39:35 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/17 21:50:11 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/23 17:53:13 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "test_map.hpp"
+#include "test_set.hpp"
 
 #define SIZE_M1 17
 #define SIZE_M2 4
 #define BIG_SIZE 100000
 
-typedef NAMESPACE::map<int, int>	MAP;
+typedef NAMESPACE::set<int>	SET;
 
 int	main	(void)
 {
-	MAP		m1, m2;
+	SET		m1, m2;
 
 	for (int i = 0; i < SIZE_M1; ++i)
-		m1[i] = -1 - i;
+		m1.insert(-1 - i);
 	for (int i = 0; i < SIZE_M2; ++i)
-		m2[-1 - i] = i;
+		m2.insert(i);
 
 	PRINT_EXECUTE(m1.size());
-	print_map(m1);
+	print_set(m1);
 	PRINT_EXECUTE(m2.size());
-	print_map(m2);
+	print_set(m2);
 
 	m1.swap(m2);
 	PRINT_EXECUTE(m1.size());
-	print_map(m1);
+	print_set(m1);
 	PRINT_EXECUTE(m2.size());
-	print_map(m2);
+	print_set(m2);
 
 	m2.swap(m1);
 	PRINT_EXECUTE(m1.size());
-	print_map(m1);
+	print_set(m1);
 	PRINT_EXECUTE(m2.size());
-	print_map(m2);
+	print_set(m2);
 
 	NAMESPACE::swap(m1, m2);
 	PRINT_EXECUTE(m1.size());
-	print_map(m1);
+	print_set(m1);
 	PRINT_EXECUTE(m2.size());
-	print_map(m2);
+	print_set(m2);
 
 	NAMESPACE::swap(m2, m1);
 	PRINT_EXECUTE(m1.size());
-	print_map(m1);
+	print_set(m1);
 	PRINT_EXECUTE(m2.size());
-	print_map(m2);
+	print_set(m2);
 
 
 	for (int i = 0; i < BIG_SIZE; ++i)
-		m1[i] = i;
+		m1.insert(i);
 	NAMESPACE::swap(m1, m2);
 
 	return (0);

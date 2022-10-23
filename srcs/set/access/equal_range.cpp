7abc83701f44eb8f84ae0bcf95fe46a60fae6ee3
Remy GENY <rgeny@e1r10p5.clusters.42paris.fr>
2022-10-23 18:00:30 +0200

    set: add test for equal_range

--- srcs/set/access/equal_range.cpp
+++ srcs/set/access/equal_range.cpp
@@ -6,17 +6,17 @@
 /*   By: rgeny <rgeny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 14:06:15 by rgeny             #+#    #+#             */
-/*   Updated: 2022/10/18 16:57:48 by rgeny            ###   ########.fr       */
+/*   Updated: 2022/10/23 18:00:06 by rgeny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "test_map.hpp"
+#include "test_set.hpp"
 
 #define SIZE 6
 
-typedef NAMESPACE::map<int, int>									MAP;
-typedef NAMESPACE::pair<MAP::iterator, MAP::iterator>				IT_PAIR;
-typedef NAMESPACE::pair<MAP::const_iterator, MAP::const_iterator>	CIT_PAIR;
+typedef NAMESPACE::set<int>											SET;
+typedef NAMESPACE::pair<SET::iterator, SET::iterator>				IT_PAIR;
+typedef NAMESPACE::pair<SET::const_iterator, SET::const_iterator>	CIT_PAIR;
 
 template < typename Container >
 void	Test<Container>::test	(void)
@@ -26,10 +26,8 @@ void	Test<Container>::test	(void)
 		int const	val = i;
 		IT_PAIR		it = this->equal_range(val);
 
-		PRINT_EXECUTE(it.first->first);
-		PRINT_EXECUTE(it.first->second);
-		PRINT_EXECUTE(it.second->first);
-		PRINT_EXECUTE(it.second->second);
+		PRINT_EXECUTE(*it.first);
+		PRINT_EXECUTE(*it.second);
 		_print_nl();
 	}
 }
@@ -41,22 +39,20 @@ void	Test<Container>::const_test	(void) const
 		int const	val = i;
 		CIT_PAIR	cit= this->equal_range(val);
 
-		PRINT_EXECUTE(cit.first->first);
-		PRINT_EXECUTE(cit.first->second);
-		PRINT_EXECUTE(cit.second->first);
-		PRINT_EXECUTE(cit.second->second);
+		PRINT_EXECUTE(*cit.first);
+		PRINT_EXECUTE(*cit.second);
 		_print_nl();
 	}
 }
 
 int	main	(void)
 {
-	Test<MAP>			m;
+	Test<SET>			m;
 	
 	for (int i = 0; i < SIZE; ++i)
 	{
-		m[i] = i;
-		m.insert(MAP::value_type(i, -i - 4));
+		m.insert(i);
+		m.insert(SET::value_type(i));
 	}
 
 	m.test();
